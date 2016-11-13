f0e39ceba84a53cfef71ea6e3f66ea29871b1be2
StevenW <StevenW.info@gmail.com>
2016-11-12 16:10:30 -0800

    Removing some unnecessary lines.

--- src/Main.hs
+++ src/Main.hs
@@ -44,7 +44,6 @@ main = hakyllWith config $ do
             posts <- fmap (take 5) . recentFirst =<< loadAll "posts/*"
             let postListingCtx = listField "posts" defaultContext (return posts) <> defaultContext
             getResourceBody
-                >>= applyAsTemplate postListingCtx
                 >>= loadAndApplyTemplate "templates/default.html" postListingCtx
                 >>= loadAndApplyTemplate "templates/boilerplate.html" postListingCtx
                 >>= relativizeUrls
@@ -55,7 +54,6 @@ main = hakyllWith config $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let postListingCtx = listField "posts" defaultContext (return posts) <> defaultContext
             getResourceBody
-                >>= applyAsTemplate postListingCtx
                 >>= loadAndApplyTemplate "templates/default.html" postListingCtx
                 >>= loadAndApplyTemplate "templates/boilerplate.html" postListingCtx
                 >>= relativizeUrls
